a894729f9fdc1ab49b03122ef7772e5d9e63aedd
Jsonghh <jiesonghe@outlook.com>
2019-12-21 22:44:41 -0500

    Added Profilefeed API

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -29,4 +29,13 @@ class UserProfileSerializer(serializers.ModelSerializer):
         return user
 
 
+class ProfileFeedItemSerializer(serializers.ModelSerializer):
+    '''Serializes prfile feed items'''
+
+    class Meta:
+        model = models.ProfileFeedItem
+        fields = ('id', 'user_profile', 'status_text', 'created_on')
+        extra_kwargs = {'user_profile': {'read_only': True}} 
+
+
 
